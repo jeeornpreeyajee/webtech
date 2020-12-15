555cadae5439f4bcc5a730e3ded34ac49b4f8c0f
jeeornpreeyajee <76036905+jeeornpreeyajee@users.noreply.github.com>
2020-12-15 16:09:02 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
       <h1>สวัสดี ศิลปากร</h1>
       <h2>web technology for data sciecne</h2>
 
-<p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล
-ภาษาอังกฤษ :Bachelor of Science Program in Data Science
-ชื่อปริญญา
-ภาษาไทย : วิทยาศาสตรบัณฑิต (วิทยาการข้อมูล)
-ภาษาอังกฤษ :Bachelor of Science (Data Science)</p>
+<p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
+<p>ภาษาอังกฤษ :Bachelor of Science Program in Data Science</p>
+<p>ชื่อปริญญา</p>
+<p>ภาษาไทย : วิทยาศาสตรบัณฑิต (วิทยาการข้อมูล)</p>
+<p>ภาษาอังกฤษ :Bachelor of Science (Data Science)</p>
 
 <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
   </body>
